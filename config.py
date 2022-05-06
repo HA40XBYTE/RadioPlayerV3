1a26196e6160addf76cd88ab791d839f4b51e3ce
Q7V4E3C <104752672+HA40XBYTE@users.noreply.github.com>
2022-05-06 22:49:21 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """
-RadioPlayerV3, Telegram Voice Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+RadioPlayerV3, Telegram Voice Chat Bot.  :):
+Copyright (c) 2022  NS <https://github.com/Q7V4E3C>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
